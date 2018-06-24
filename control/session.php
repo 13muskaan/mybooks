0bdfbc19022b60ad531353969e27bd77b549db07
Muskaan Bakshi <13muskanbakshi@gmail.com>
2018-06-24 16:03:37 +1000

    intentional error to check.

--- control/session.php
+++ control/session.php
@@ -10,4 +10,20 @@
 
 // destroy
 
+if (isset($_SESSION['Login']) == true) {
+ if ($_GET['role'] == '1') {
+ 	client_portal();
+ } else if ($_GET['role'] == '2'){ 		
+    Admin_portal();   mlmlm
+ }
+ } else {
+	session_destroy();
+ 	header('location:../view/html/login.php');
+ }
+  function client_portal(){
+ 	header('location:../view/html/user.php');
+ }
+ function Admin_portal(){
+ 	header('location:../view/html/admin.php');
+ }
 ?>
