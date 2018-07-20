90931f077cceab74ee228919244c160512c75ede
Muskaan Bakshi <13muskanbakshi@gmail.com>
2018-07-20 12:02:10 +1000

    updated view books page

--- view/pages/viewbooks.php
+++ view/pages/viewbooks.php
@@ -24,82 +24,54 @@
 		</div>
 	</div>
 	<div class="container">
-		<div class="row">
-			<div class="col-sm-4">
-				<div class="panel panel-primary">
-					<?php 
+		<?php 
 		  
 		  $contentquery = "SELECT * FROM book";
 		  $stmt = $conn->prepare($contentquery);
 		  $stmt->execute();
 		  $staticresult = $stmt->fetchAll(PDO::FETCH_ASSOC);
-		  foreach($staticresult as $row){}
-		  
+					
+		  echo '<div class="row">';
+		
+		$count = -1;
+		
+		
+		  foreach($staticresult as $row) {
+			  
+			  if ($count % 3 == 2) {
+				  echo '</div> <div class="row">';
+			  }
+			  
+			  
+		  echo'<div class="col-sm-4"> 
+		  			<div class="panel panel-primary">';
 		  echo'<div class="panel-heading">', '<p>', 'Book Title', '</p>',$row['BookTitle'], '</div>';
 		  echo'<div class="panel-heading">', '<p>', 'Year of Publication', '</p>', $row['YearofPublication'], '</div>';
 		  echo'<div class="panel-heading">', '<p>', 'Genre', '</p>', $row['Genre'], '</div>';
 		  echo'<div class="panel-heading">', '<p>', 'Language', '</p>', $row['LanguageWritten'], '</div>';
 		  echo '<div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image"></div>';
 		  echo '<div class="panel-footer">', '<p>', 'Millions Sold: ', $row['MillionsSold'], '</p>','</div>';
-?>
-	<a href="#" class="btn btn-primary a-btn-slide-text">
+			  
+			echo '<a href="#" class="btn btn-primary a-btn-slide-text">
        <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
         <span><strong>Delete</strong></span>            
     </a>
-	<a href="updatebooks.php" class="btn btn-primary a-btn-slide-text">
+				
+					<a href="updatebooks.php?name=" class="btn btn-primary a-btn-slide-text">
        <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>
         <span><strong>Edit</strong></span>            
-    </a>
-				
-				</div>
-			</div>
-			<div class="col-sm-4">
-				<div class="panel panel-danger">
-					<div class="panel-heading">BLACK FRIDAY DEAL</div>
-					<div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
-					</div>
-					<div class="panel-footer">Buy 50 mobiles and get a gift card</div>
-				</div>
-			</div>
-			<div class="col-sm-4">
-				<div class="panel panel-success">
-					<div class="panel-heading">BLACK FRIDAY DEAL</div>
-					<div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
-					</div>
-					<div class="panel-footer">Buy 50 mobiles and get a gift card</div>
-				</div>
-			</div>
-		</div>
-	</div><br>
-
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-4">
-				<div class="panel panel-primary">
-					<div class="panel-heading">BLACK FRIDAY DEAL</div>
-					<div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
-					</div>
-					<div class="panel-footer">Buy 50 mobiles and get a gift card</div>
-				</div>
-			</div>
-			<div class="col-sm-4">
-				<div class="panel panel-primary">
-					<div class="panel-heading">BLACK FRIDAY DEAL</div>
-					<div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
-					</div>
-					<div class="panel-footer">Buy 50 mobiles and get a gift card</div>
-				</div>
-			</div>
-			<div class="col-sm-4">
-				<div class="panel panel-primary">
-					<div class="panel-heading">BLACK FRIDAY DEAL</div>
-					<div class="panel-body"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
-					</div>
-					<div class="panel-footer">Buy 50 mobiles and get a gift card</div>
-				</div>
-			</div>
-		</div>
-	</div><br><br>
+    </a>';
+		
+			echo '</div>
+			</div>';
+			  
+			  $count++;
+		  }
+		
+?>
+	</div>
+	</div>
+	<br><br>
 
 	<footer class="container-fluid text-center">
 		<?php include('footer.php');?>
