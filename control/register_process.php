3020083217465a44ad149965c5165e4671e2095b
Muskaan Bakshi <13muskanbakshi@gmail.com>
2018-08-11 19:42:06 +1000

    deleted unwanted code from register_process.

--- control/register_process.php
+++ control/register_process.php
@@ -14,7 +14,7 @@ $_SESSION[ 'error' ] = "";
 $hash = password_hash( $password, PASSWORD_DEFAULT );
 
 
-if ( empty( $email ) || empty( $password ) || empty( $firstname ) || empty( $lastname ) || empty( $password ) ) {
+if ( empty( $email ) || empty( $password ) || empty( $firstname ) || empty( $lastname ) ) {
 	$_SESSION[ 'error' ] = "Please fill in all the input fields.";
 } else {
 
@@ -22,27 +22,27 @@ if ( empty( $email ) || empty( $password ) || empty( $firstname ) || empty( $las
 	if ( !$firstname_check ) {
 		$_SESSION[ 'error' ] .= "\n<hr>First Name is not formatted correctly, Make sure there is:\n-at least three characters\n-No numbers\n-No symbols (except hyphen '-')";
 	}
-	
+
 	$lastname_check = preg_match( '~^[A-Za-z\-]{3,20}$~i', $lastname );
 	if ( !$lastname_check ) {
 		$_SESSION[ 'error' ] .= "\n<hr>Last Name is not formatted correctly, Make sure there is:\n-at least three characters\n-No numbers\n-No symbols (except hyphen '-')";
 	}
-	
+
 	$email_check = preg_match( '~^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.([a-zA-Z]{2,4})$~i', $email );
 	if ( !$email_check ) {
 		$_SESSION[ 'error' ] .= "\n<hr> Email is not formatted correctly, Make sure there is:\n- @ in the email \n- A mail-to address eg. (domain).com";
 	}
-	
+
 	$emailSQL = "SELECT * FROM users WHERE email = :email";
-	
-	$stmt = $conn->prepare($emailSQL);
+
+	$stmt = $conn->prepare( $emailSQL );
 	$stmt->bindParam( ':email', $email, PDO::PARAM_STR );
 	$stmt->execute();
-	
-	if ($stmt->rowcount() > 0) {
-		$_SESSION['error'] = "<hr>This email is already registered.";
+
+	if ( $stmt->rowcount() > 0 ) {
+		$_SESSION[ 'error' ] = "<hr>This email is already registered.";
 	}
-	
+
 	if ( $_SESSION[ 'error' ] == "" ) {
 
 		$register_sql = "INSERT INTO users (email, password, firstname, lastname ) VALUES (:email, :password, :firstname, :lastname)";
